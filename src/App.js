c098cc98ec4acaac2512967fa03bd5327ef7b743
macRos-Alzoubi <macRos.alzoubi@gmail.com>
2021-07-27 15:57:27 +0300

    finish lab03

--- src/App.js
+++ src/App.js
@@ -2,14 +2,53 @@ import React from 'react';
 import Header from './Component/Header';
 import Main from './Component/Main';
 import Footer from './Component/Footer';
-// import './Component/HornedBeast.css';
+import Beasts from './Component/Beasts.json';
+import SelectedBeast from './Component/SelectedBeast';
+
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      title: 'title',
+      imageUrl: 'url',
+      description: 'des',
+      modalState: false,
+    };
+  }
+
+
+  modalHandler = (title, imageUrl, description, modalState) => {
+      this.setState({
+        title: title,
+        imageUrl: imageUrl,
+        description: description,
+        modalState: modalState,
+      });
+  };
+
+  handelModalShow = modalState =>{
+      this.setState({
+        modalState: modalState
+      });
+  }
+
+
   render(){
     return(
       <>
+        <SelectedBeast 
+          title={this.state.title}
+          imageUrl={this.state.imageUrl}
+          description={this.state.description}
+          showModal={this.state.modalState}
+          hideModal={this.handelModalShow}
+
+        />
         <Header />
-        <Main />
+        <Main 
+          beastList={Beasts}
+          modalHandler={this.modalHandler}/>
         <Footer />
       </>
     );
--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import HornedBeasts from './HornedBeast';
-import Beasts from './Beasts.json';
 import Row from 'react-bootstrap/Row';
 import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Main extends React.Component{
     render(){
         return(
             <>
                 <Container>
-                    <Row xs={1} md={3} className='justify-content-center align-items-center'>
+                    <Row xs={1} md={3} className='justify-content-center'>
                         {
-                            Beasts.map(bestObj => {
+                            this.props.beastList.map(bestObj => {
                                 return <HornedBeasts 
                                             key={bestObj.title.toString()}
                                             imageUrl={bestObj.image_url}
                                             title={bestObj.title}
                                             description={bestObj.description}
+                                            modalHandler={this.props.modalHandler}
                                         />
                             })
                         }
