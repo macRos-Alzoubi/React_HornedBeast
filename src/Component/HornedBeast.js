1c1fc7bd858b4e2b5dad2025417390df6c08faed
macRos-Alzoubi <macRos.alzoubi@gmail.com>
2021-07-27 11:56:46 +0300

    use React Bootstrap Grid

--- src/Component/HornedBeast.js
+++ src/Component/HornedBeast.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import './component.css';
+import Col from 'react-bootstrap/Col';
 
 class HornedBeasts extends React.Component{
 
@@ -23,17 +23,19 @@ class HornedBeasts extends React.Component{
     render(){
         return(
             <>
-                <Card style={{ width: '18rem' }} className='card'>
-                    <Card.Img onClick={this.votesHandler} variant="top" src={this.props.imageUrl} className='best-img'/>
-                    <Card.Body>
-                        <Card.Title>{this.props.title}</Card.Title>
-                        <Card.Text>
-                        ❤ {this.state.numberOfVotes}
-                        </Card.Text>
-                        <Card.Footer>{this.props.description}</Card.Footer>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>              
+                <Col>
+                    <Card style={{ width: '18rem' }} className='card'>
+                        <Card.Img onClick={this.votesHandler} variant="top" src={this.props.imageUrl} className='best-img'/>
+                        <Card.Body>
+                            <Card.Title>{this.props.title}</Card.Title>
+                            <Card.Text>
+                            ❤ {this.state.numberOfVotes}
+                            </Card.Text>
+                            <Card.Footer>{this.props.description}</Card.Footer>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>      
+                </Col>        
             </>
         );
     }
--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import HornedBeasts from './HornedBeast';
 import Beasts from './Beasts.json';
-import './component.css';
+import Row from 'react-bootstrap/Row';
+import { Container } from 'react-bootstrap';
 
 class Main extends React.Component{
     render(){
         return(
             <>
-                <div className='card-container'>
-                    {
-                        Beasts.map(bestObj => {
-                            return <HornedBeasts 
-                                        imageUrl={bestObj.image_url}
-                                        title={bestObj.title}
-                                        description={bestObj.description}
-                                    />
-                        })
-                    }
-                </div>
+                <Container>
+                    <Row xs={1} md={3} className='justify-content-center align-items-center'>
+                        {
+                            Beasts.map(bestObj => {
+                                return <HornedBeasts 
+                                            key={bestObj.title.toString()}
+                                            imageUrl={bestObj.image_url}
+                                            title={bestObj.title}
+                                            description={bestObj.description}
+                                        />
+                            })
+                        }
+                    </Row>
+                </Container>
             </>
         );
     }
